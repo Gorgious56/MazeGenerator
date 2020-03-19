77fcf476f073a01fab55b8e07587fee0f9d7d926
Nathan Hild <nathan.hild@gmail.com>
2020-03-19 13:46:31 +0100

    add properties for walls dimensions and seed

--- Scripts/mg_properties.py
+++ Scripts/mg_properties.py
@@ -1,6 +1,6 @@
 from bpy.types import PropertyGroup, Scene
-from bpy.props import IntProperty, BoolProperty, EnumProperty, PointerProperty
-from . algorithm_names import generate_algo_enum, DEFAULT_ALGO
+from bpy.props import IntProperty, BoolProperty, EnumProperty, FloatProperty, PointerProperty
+from . maze_logic . algorithms . algorithm_manager import generate_algo_enum, DEFAULT_ALGO
 
 
 class MGProperties(PropertyGroup):
@@ -22,6 +22,28 @@ class MGProperties(PropertyGroup):
         default=5,
         min=1,
         soft_max=100
+    )
+
+    seed: IntProperty(
+        name="Seed",
+        description="This seed will be used to randomize the maze",
+        default=0,
+        # update=lambda self, context: print(context.seed)
+    )
+
+    wall_height: FloatProperty(
+        name="Height",
+        description="Configure the wall default height",
+        default=0.5,
+        min=0
+    )
+
+    wall_width: FloatProperty(
+        name="Width",
+        description="Configure the wall default width",
+        default=0.2,
+        min=0,
+        soft_max=2
     )
 
     def register():
