f5b98828da81a0b81c3dca2abe0c1344b53d5eab
Nathan Hild <nathan.hild@gmail.com>
2020-03-19 13:45:40 +0100

    add base data structure

---
+++ Scripts/maze_logic/data_structure/cell.py
@@ -0,0 +1,56 @@
+class Cell:
+    neighbors_return = [2, 3, 0, 1]
+    neighbors_name = ["North", "West", "South", "East"]
+
+    def __init__(self, row, col):
+        self.row = row
+        self.column = col
+
+        self.group = 0
+
+        self.neighbors = [None] * 4
+
+        self.links = {}
+
+        self.is_masked = False
+
+    def __str__(self):
+        return 'Cell(r' + str(self.row) + ';c' + str(self.column) + ')'
+
+    def __repr__(self):
+        return self.__str__()
+
+    def link(self, other_cell, bidirectional=True):
+        self.links[other_cell] = True
+        if bidirectional:
+            other_cell.link(self, False)
+
+    def unlink(self, other_cell, bidirectional=True):
+        try:
+            del self.links[other_cell]
+        except KeyError:
+            pass
+        if bidirectional:
+            other_cell.unlink(self, False)
+
+    def is_linked(self, other_cell):
+        return other_cell in self.links
+
+    def has_any_link(self):
+        return any(self.links)
+
+    def get_neighbors(self):
+        return [n for n in self.neighbors if n]
+
+    def get_unlinked_neighbors(self):
+        return [c for c in self.get_neighbors() if not c.has_any_link()]
+
+    def get_linked_neighbors(self):
+        return [c for c in self.get_neighbors() if c.has_any_link()]
+
+    def exists_and_is_linked(self, other_cell):
+        return other_cell is not None and other_cell in self.links
+
+    # Get the wall mask.
+    def get_wall_mask(self):
+        return [not self.exists_and_is_linked(n) for n in self.neighbors] if self.has_any_link() else [False] * len(self.neighbors)
---
+++ Scripts/maze_logic/data_structure/grid.py
@@ -0,0 +1,167 @@
+from . import cell
+from mathutils import Vector
+from random import choice, seed, shuffle
+
+
+class Grid:
+    def __init__(self, rows, columns, name="", coord_system='cartesian', sides=4):
+        self.rows = rows
+        self.columns = columns
+        self.cells = [None] * (rows * columns)
+        self.size = rows * columns
+        self.masked_cells = []
+        self.name = name
+
+        self.coord_system = coord_system
+
+        self.sides_per_cell = sides
+
+        self.prepare_grid()
+        self.configure_cells()
+
+        self.offset = (columns / 2, rows / 2, 0)
+
+    def __delitem__(self, key):
+        del self.cells[key[0] + key[1] * self.columns]
+
+    def __getitem__(self, key):
+        return self.cells[key[0] + key[1] * self.columns] \
+            if (self.columns > key[0] >= 0 and self.rows > key[1] >= 0) else None
+
+    def __setitem__(self, key, value):
+        self.cells[key[0] + key[1] * self.columns] = value
+
+    def prepare_grid(self):
+        for c in range(self.columns):
+            for r in range(self.rows):
+                self[c,r] = cell.Cell(r, c)
+
+    def configure_cells(self):
+        for c in self.cells:
+            row, col = c.row, c.column
+
+            # North :
+            c.neighbors[0] = self[col, row + 1]
+            # West :
+            c.neighbors[1] = self[col-1, row]
+            # South :
+            c.neighbors[2] = self[col, row - 1]
+            # East :
+            c.neighbors[3] = self[col+1, row]
+
+    def random_cell(self, _seed=None, filter_mask=True):
+        if _seed:
+            seed(_seed)
+        try:
+            return choice(self.get_unmasked_cells()) if filter_mask else choice(self.cells)
+        except IndexError:
+            return None
+
+    def each_row(self):
+        cols = self.columns
+        for r in range(self.rows):
+            yield [c for c in self.cells[r * cols: (r + 1) * cols] if not c.is_masked]
+
+    def each_column(self):
+        cols = self.columns
+        for c in range(cols):
+            yield [self.cells[r * cols + c] for r in range(self.rows) if not self.cells[r * cols + c].is_masked]
+
+    def each_cell(self):
+        for c in self.get_unmasked_cells():
+            yield c
+
+    def get_dead_ends(self):
+        return [c for c in self.get_unmasked_cells() if len(c.links) == 1]
+
+    def braid_dead_ends(self, braid=0):
+        dead_ends_shuffle = self.get_dead_ends()
+        shuffle(dead_ends_shuffle)
+        stop_index = int(len(dead_ends_shuffle) * min(max(0, braid), 1))
+        for c in dead_ends_shuffle[0:stop_index]:
+            if len(c.links) != 1:
+                pass
+            else:
+                unconnected_neighbors = [_c for _c in c.get_neighbors() if _c not in c.links]
+                if len(unconnected_neighbors) > 0:
+                    best = [_c for _c in unconnected_neighbors if len(c.links) < 2]
+                    if len(best) == 0:
+                        best = unconnected_neighbors
+                    c.link(choice(best))
+        return dead_ends_shuffle[stop_index:len(dead_ends_shuffle)]
+
+    def mask_cell(self, column, row):
+        c = self[column, row]
+        if c is not None:
+            c.is_masked = True
+            self.masked_cells.append(c)
+            for i, n in enumerate(c.get_neighbors()):
+                n.neighbors[c.neighbors_return[i]] = None
+
+    def mask_ring(self, center_row, center_col, radius):     
+        for r in range(self.rows):
+            for c in range(self.columns):
+                if ((center_col - c) ** 2 + (center_row - r) ** 2) ** 0.5 > radius:
+                    self.mask_cell(c, r)
+
+    def get_unmasked_cells(self):
+        return [c for c in self.cells if not c.is_masked]
+
+    def get_random_unmasked_and_linked_cell(self, _seed=None):
+        if _seed:
+            seed(_seed)
+        try:
+            return choice(self.get_unmasked_and_linked_cells())
+        except IndexError:
+            return None
+
+    def get_unmasked_and_linked_cells(self):
+        return [c for c in self.each_cell() if any(c.links)]
+
+    def is_cell_linked(self, c):
+        return c and any(c.links)
+
+    def get_cell_position(self, c, cell_size):
+        offset = self.offset * cell_size
+        # center = Vector([c.column * cell_size, c.row * cell_size, 0]) - offset
+        # top_left = Vector([(c.column - 0.5) * cell_size, (c.row + 0.5) * cell_size, 0]) - offset
+        # top_right = Vector([(c.column + 0.5) * cell_size, (c.row + 0.5) * cell_size, 0]) - offset
+        # bot_right = Vector([(c.column + 0.5) * cell_size, (c.row - 0.5) * cell_size, 0]) - offset
+        # bot_left = Vector([(c.column - 0.5) * cell_size, (c.row - 0.5) * cell_size, 0]) - offset
+        center = self.sub_vec((c.column * cell_size, c.row * cell_size, 0), offset)
+        top_left = self.sub_vec(((c.column - 0.5) * cell_size, (c.row + 0.5) * cell_size, 0), offset)
+        top_right = self.sub_vec(((c.column + 0.5) * cell_size, (c.row + 0.5) * cell_size, 0), offset)
+        bot_right = self.sub_vec(((c.column + 0.5) * cell_size, (c.row - 0.5) * cell_size, 0), offset)
+        bot_left = self.sub_vec(((c.column - 0.5) * cell_size, (c.row - 0.5) * cell_size, 0), offset)
+        return center, top_left, top_right, bot_right, bot_left
+
+    def sub_vec(self, vec1, vec2):
+        return (vec1[0] - vec2[0], vec1[1] - vec2[1], vec1[2] - vec2[2])
+
+    def get_cell_rotation(self, c):
+        return Vector([0, 0, 45])
+
+    def get_all_walls(self):
+        walls = []
+        for c in self.cells:
+            walls.extend(self.get_cell_walls(c))
+        return walls
+
+    def get_cell_walls(self, c):
+        walls = []
+        mask = c.get_wall_mask()
+        positions = self.get_cell_position(c, 1)
+        if mask[0]:
+            walls.append(positions[1])
+            walls.append(positions[2])
+        if mask[1]:
+            walls.append(positions[1])
+            walls.append(positions[4])
+        if mask[2]:
+            walls.append(positions[3])
+            walls.append(positions[4])
+        if mask[3]:
+            walls.append(positions[2])
+            walls.append(positions[3])
+        return walls
+
