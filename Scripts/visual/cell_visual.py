55206ec7963788979a80f3840a90f757f9bbef04
Nathan Hild <nathan.hild@gmail.com>
2020-03-28 13:49:00 +0100

    simplify cell visual class

--- Scripts/visual/cell_visual.py
+++ Scripts/visual/cell_visual.py
@@ -1,35 +1,22 @@
-class Face:
-    def __init__(self):
-        self.vertices = []
-        self.vertices_indexes = []
-        self.vertices_levels = []
-        self.edges = []
-        self.face = None
-        self.vertex_groups = None
+DISTANCE = 'DISTANCE'
+GROUP = 'GROUP'
+UNIFORM = 'UNIFORM'
+NEIGHBORS = 'NEIGHBORS'
 
-    def corners(self):
-        return len(self.vertices)
+DEFAULT_CELL_VISUAL_TYPE = DISTANCE
 
-    def add_vertex(self, vec, level):
-        self.vertices.append(vec)
-        self.vertices_levels.append(level)
 
-    def add_edges(self, delta):
-        self.edges = [(delta + i, delta + ((i + 1) % self.corners())) for i in range(self.corners())]
+def generate_cell_visual_enum():
+    return [(DISTANCE, 'Distance', ''),
+            (GROUP, 'Cell Group', ''),
+            (UNIFORM, 'Uniform', ''),
+            (NEIGHBORS, 'Neighbors Amount', ''),
+            ]
 
-    def add_face(self, delta):
-        self.face = [delta + i for i in range(len(self.edges))]
 
-    def translate(self, delta):
-        self.add_edges(delta)
-        self.add_face(delta)
-        self.vertices_indexes = [i + delta for i in range(len(self.vertices))]
+DISPLACE = 'DISPLACE'
 
-    def set_vertex_group(self, vertex_group_name, weights):
-        if self.vertex_groups:
-            self.vertex_groups[vertex_group_name] = weights
-        else:
-            self.vertex_groups = {vertex_group_name: weights}
+VERTEX_GROUPS = DISPLACE, 
 
 
 class CellVisual:
@@ -56,9 +43,42 @@ class CellVisual:
     def add_face(self, vertices, vertices_levels=None):
         if vertices_levels is None:
             vertices_levels = [0 for i in range(len(vertices))]
-        f = Face()
+        f = CellVisual.Face()
         [f.add_vertex(v, v_level) for v, v_level in zip(vertices, vertices_levels)]
         self.faces.append(f)
 
     def get_faces_with_vertex_weights(self):
-        return [f for f in self.faces if f.vertex_groups is not None]
+        return [f for f in self.faces if f.vertex_groups is not None]    
+
+    class Face:
+        def __init__(self):
+            self.vertices = []
+            self.vertices_indexes = []
+            self.vertices_levels = []
+            self.edges = []
+            self.face = None
+            self.vertex_groups = None
+
+        def corners(self):
+            return len(self.vertices)
+
+        def add_vertex(self, vec, level):
+            self.vertices.append(vec)
+            self.vertices_levels.append(level)
+
+        def add_edges(self, delta):
+            self.edges = [(delta + i, delta + ((i + 1) % self.corners())) for i in range(self.corners())]
+
+        def add_face(self, delta):
+            self.face = [delta + i for i in range(len(self.edges))]
+
+        def translate(self, delta):
+            self.add_edges(delta)
+            self.add_face(delta)
+            self.vertices_indexes = [i + delta for i in range(len(self.vertices))]
+
+        def set_vertex_group(self, vertex_group_name, weights):
+            if self.vertex_groups:
+                self.vertex_groups[vertex_group_name] = weights
+            else:
+                self.vertex_groups = {vertex_group_name: weights}
