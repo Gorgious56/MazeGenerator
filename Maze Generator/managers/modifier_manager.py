3610d713ac69ac47d600403a63cf832e5842be67
Nathan Hild <nathan.hild@gmail.com>
2020-05-03 17:18:21 +0200

    Mask out cells outside of longest path

--- Maze Generator/managers/modifier_manager.py
+++ Maze Generator/managers/modifier_manager.py
@@ -27,6 +27,7 @@ M_STAIRS = 'MG_STAIRS'
 M_TEXTURE_DISP = 'MG_TEX_DISP'
 M_MASK = 'MG_MASK_STAIRS'
 M_MASK_SPARSE = 'MG_MASK_SPARSE'
+M_MASK_LONGEST_PATH = 'MG_MASK_PATH'
 M_VERT_WEIGHT_PROX = 'MG_WEIGHT_PROX'
 M_SMOOTH_CENTER_NAME = 'MG_SMOOTH_CENTER'
 M_SMOOTH_BRIDGE_COL_X_NAME, M_SMOOTH_BRIDGE_COL_Y_NAME = 'MG_SMOOTH_BRIDGE_COL_X', 'MG_SMOOTH_BRIDGE_COL_Y'
@@ -37,7 +38,7 @@ VISIBILIY = 'VISIBILITY'
 
 
 def setup_modifiers_and_drivers(MV, OM, TM) -> None:
-    ow = MV.props.auto_overwrite
+    ow = MV.props.auto_overwrite or not any(ModifierManager.drivers)
     obj_walls = OM.obj_walls
     obj_cells = OM.obj_cells
     obj_cylinder = OM.obj_cylinder
@@ -47,12 +48,15 @@ def setup_modifiers_and_drivers(MV, OM, TM) -> None:
     scene = MV.scene
     mod_dic = {
         obj_walls: (
-            ('MASK', M_MASK_SPARSE, {
-                VISIBILIY: ('sparse_dead_ends', 'var > 0'),
-                'vertex_group': cv.VG_STAIRS,
+            ('MASK', M_MASK_LONGEST_PATH, {
+                VISIBILIY: (obj_cells, obj_walls, 'show_viewport', M_MASK_LONGEST_PATH, M_MASK_LONGEST_PATH, 'var'),
+                'vertex_group': cv.VG_LONGEST_PATH,
                 'invert_vertex_group': True,
-                'threshold': 1
-            }), 
+            }),
+            ('MASK', M_MASK_SPARSE, {
+                'vertex_group': cv.VG_STAIRS,
+                'threshold': 0
+            }),
             ('MASK', M_MASK, {
                 VISIBILIY: (obj_walls, obj_walls, 'threshold', M_MASK, M_MASK, 'var < 1'),
                 'vertex_group': cv.VG_STAIRS,
@@ -75,21 +79,27 @@ def setup_modifiers_and_drivers(MV, OM, TM) -> None:
                 'screw_offset': 0.3,
                 'use_smooth_shade': ('wall_bevel', 'var > 0.005'),
             }),
+            ('VERTEX_WEIGHT_PROXIMITY', M_VERT_WEIGHT_PROX, {
+                'vertex_group': cv.VG_THICKNESS,
+                'target': obj_cells,
+                'proximity_mode': 'GEOMETRY',
+                'proximity_geometry': {'VERTEX'},
+                'falloff_type': 'STEP',
+                'min_dist': 0.001,
+                'max_dist': 0,
+            }),
             ('SOLIDIFY', M_SOLID, {
                 VISIBILIY: (obj_walls, obj_walls, 'thickness', M_SOLID, M_SOLID, 'var != 0'),
                 'solidify_mode': 'NON_MANIFOLD',
                 'thickness': 0.1,
                 'offset': 0,
             }),
-            ('VERTEX_WEIGHT_PROXIMITY', M_VERT_WEIGHT_PROX, {
-                VISIBILIY: ('cell_thickness_equalize', 'var'),
+            ('DISPLACE', M_THICKNESS_DISP, {
+                VISIBILIY: ('cell_thickness_equalize', 'not var'),
+                'direction': 'Z',
                 'vertex_group': cv.VG_THICKNESS,
-                'target': obj_cells,
-                'proximity_mode': 'GEOMETRY',
-                'proximity_geometry': {'VERTEX'},
-                'falloff_type': 'STEP',
-                'min_dist': 0.3,
-                'max_dist': 0,
+                'mid_level': 0,
+                'strength': (obj_cells, obj_walls, 'strength', M_THICKNESS_DISP, M_THICKNESS_DISP, 'var'),
             }),
             ('SHRINKWRAP', M_THICKNESS_SHRINKWRAP, {
                 VISIBILIY: ('cell_thickness_equalize', 'var'),
@@ -141,11 +151,14 @@ def setup_modifiers_and_drivers(MV, OM, TM) -> None:
             }),
         ),
         obj_cells: (
-            ('MASK', M_MASK_SPARSE, {
-                VISIBILIY: ('sparse_dead_ends', 'var > 0'),
-                'vertex_group': cv.VG_STAIRS,
+            ('MASK', M_MASK_LONGEST_PATH, {
+                'show_viewport': False,
+                'show_render': (obj_cells, obj_cells, 'show_viewport', M_MASK_LONGEST_PATH, M_MASK_LONGEST_PATH, 'var'),
+                'vertex_group': cv.VG_LONGEST_PATH,
                 'invert_vertex_group': True,
-                'threshold': 1
+            }),
+            ('MASK', M_MASK_SPARSE, {
+                'vertex_group': cv.VG_STAIRS,
             }),            
             ('MASK', M_MASK, {
                 VISIBILIY: (obj_cells, obj_cells, 'threshold', M_MASK, M_MASK, 'var < 1'),
@@ -253,7 +266,7 @@ def setup_modifiers_and_drivers(MV, OM, TM) -> None:
                 'material': ('cell_contour_black', '1 if var else 0'),
                 'profile': ('cell_contour_black', '1 if var else 0.5'),
                 'angle_limit': 0.75,
-                # 'use_clamp_overlap': False,
+                'use_clamp_overlap': False,
                 'width': 0,
             }),
             ('WIREFRAME', M_WIREFRAME, {
@@ -273,54 +286,56 @@ def setup_modifiers_and_drivers(MV, OM, TM) -> None:
             params[2]['show_expanded'] = False
             add_modifier(obj=obj, mod_type=params[0], mod_name=params[1], properties=params[2], overwrite_props=ow, scene=scene)
 
-    add_driver_to_vars(
-        obj_thickness_shrinkwrap,
-        ('location', 2),
-        (
-            ('stairs', 'OBJECT', obj_cells, 'modifiers["' + M_STAIRS + '"].strength'),
-            ('thickness', 'OBJECT', obj_cells, 'modifiers["' + M_THICKNESS_DISP + '"].strength'),
-        ),
-        expression='thickness if stairs > 0 else stairs + thickness',
-    )
+    if ow:
+        add_driver_to_vars(
+            obj_thickness_shrinkwrap,
+            ('location', 2),
+            (
+                ('stairs', 'OBJECT', obj_cells, 'modifiers["' + M_STAIRS + '"].strength'),
+                ('thickness', 'OBJECT', obj_cells, 'modifiers["' + M_THICKNESS_DISP + '"].strength'),
+            ),
+            expression='thickness if stairs > 0 else stairs + thickness',
+        )
 
-    # Scale the cylinder and torus objects when scaling the size of the maze
-    for i, obj in enumerate((obj_cylinder, obj_torus)):
-        drvList = obj.driver_add('scale')
-        for fc in drvList:
-            drv = fc.driver
-            try:
-                var = drv.variables[0]
-            except IndexError:
-                var = drv.variables.new()
+        # Scale the cylinder and torus objects when scaling the size of the maze
+        for i, obj in enumerate((obj_cylinder, obj_torus)):
+            drvList = obj.driver_add('scale')
+            for fc in drvList:
+                drv = fc.driver
+                try:
+                    var = drv.variables[0]
+                except IndexError:
+                    var = drv.variables.new()
 
-            var.name = 'var'
-            var.type = 'SINGLE_PROP'
+                var.name = 'var'
+                var.type = 'SINGLE_PROP'
 
-            target = var.targets[0]
-            target.id_type = 'SCENE'
-            target.id = scene
-            target.data_path = 'mg_props.maze_columns' if i == 0 else 'mg_props.maze_rows_or_radius'
+                target = var.targets[0]
+                target.id_type = 'SCENE'
+                target.id = scene
+                target.data_path = 'mg_props.maze_columns' if i == 0 else 'mg_props.maze_rows_or_radius'
 
-            exp = 'var * 0.314'
-            if MV.props.cell_type == SQUARE:
-                exp = 'var * 0.15915'
-            elif MV.props.cell_type == TRIANGLE:
-                if i == 0:
-                    exp = 'var * 0.07963'
-                else:
-                    exp = 'var * 0.13791'
-            elif MV.props.cell_type == HEXAGON:
-                if i == 0:
-                    exp = 'var * 0.2388'
-                else:
-                    exp = 'var * 0.2758'
+                exp = 'var * 0.314'
+                if MV.props.cell_type == SQUARE:
+                    exp = 'var * 0.15915'
+                elif MV.props.cell_type == TRIANGLE:
+                    if i == 0:
+                        exp = 'var * 0.07963'
+                    else:
+                        exp = 'var * 0.13791'
+                elif MV.props.cell_type == HEXAGON:
+                    if i == 0:
+                        exp = 'var * 0.2388'
+                    else:
+                        exp = 'var * 0.2758'
 
-            drv.expression = exp
+                drv.expression = exp
 
 
-def add_modifier(obj: bpy_types.Object, mod_type: str, mod_name: str,
-                 remove_if_already_exists: bool = False, remove_all_modifiers: bool = False,
-                 properties=None, overwrite_props: bool = True, scene: bpy_types.Scene = None) -> None:
+def add_modifier(
+        obj: bpy_types.Object, mod_type: str, mod_name: str,
+        remove_if_already_exists: bool = False, remove_all_modifiers: bool = False,
+        properties=None, overwrite_props: bool = True, scene: bpy_types.Scene = None) -> None:
     mod_name = mod_name if mod_name != "" else "Fallback"
 
     if obj is not None:
@@ -336,30 +351,30 @@ def add_modifier(obj: bpy_types.Object, mod_type: str, mod_name: str,
                 mod = obj.modifiers[mod_name]
         else:
             mod = obj.modifiers.new(mod_name, mod_type)
-            if mod and type(properties) is dict and overwrite_props:
-                for prop, value in properties.items():
-                    if type(value) is tuple:
-                        if type(value[0]) is not str:
-                            if type(value[2]) is tuple:
-                                if prop == VISIBILIY:
-                                    add_driver_to_vars(value[0].modifiers[value[1]], 'show_render', value[2], value[3])
-                                    add_driver_to_vars(value[0].modifiers[value[1]], 'show_viewport', value[2], value[3])
-                                else:
-                                    add_driver_to_vars(value[0].modifiers[value[1]], prop, value[2], value[3])
+        if mod and type(properties) is dict and overwrite_props:
+            for prop, value in properties.items():
+                if type(value) is tuple:
+                    if type(value[0]) is not str:
+                        if type(value[2]) is tuple:
+                            if prop == VISIBILIY:
+                                add_driver_to_vars(value[0].modifiers[value[1]], 'show_render', value[2], value[3])
+                                add_driver_to_vars(value[0].modifiers[value[1]], 'show_viewport', value[2], value[3])
                             else:
-                                if prop == VISIBILIY:
-                                    add_driver_to(value[1].modifiers[value[4]], 'show_render', 'var', 'OBJECT', value[0], 'modifiers["' + value[3] + '"].' + value[2], value[5])
-                                    add_driver_to(value[1].modifiers[value[4]], 'show_viewport', 'var', 'OBJECT', value[0], 'modifiers["' + value[3] + '"].' + value[2], value[5])
-                                else:
-                                    add_driver_to(value[1].modifiers[value[4]], prop, 'var', 'OBJECT', value[0], 'modifiers["' + value[3] + '"].' + value[2], value[5])
+                                add_driver_to_vars(value[0].modifiers[value[1]], prop, value[2], value[3])
                         else:
                             if prop == VISIBILIY:
-                                add_driver_to(obj.modifiers[mod_name], 'show_render', 'var', 'SCENE', scene, 'mg_props.' + value[0], expression=value[1])
-                                add_driver_to(obj.modifiers[mod_name], 'show_viewport', 'var', 'SCENE', scene, 'mg_props.' + value[0], expression=value[1])
+                                add_driver_to(value[1].modifiers[value[4]], 'show_render', 'var', 'OBJECT', value[0], 'modifiers["' + value[3] + '"].' + value[2], value[5])
+                                add_driver_to(value[1].modifiers[value[4]], 'show_viewport', 'var', 'OBJECT', value[0], 'modifiers["' + value[3] + '"].' + value[2], value[5])
                             else:
-                                add_driver_to(obj.modifiers[mod_name], prop, 'var', 'SCENE', scene, 'mg_props.' + value[0], expression=value[1])
-                    elif hasattr(mod, prop):
-                        setattr(mod, prop, value)
+                                add_driver_to(value[1].modifiers[value[4]], prop, 'var', 'OBJECT', value[0], 'modifiers["' + value[3] + '"].' + value[2], value[5])
+                    else:
+                        if prop == VISIBILIY:
+                            add_driver_to(obj.modifiers[mod_name], 'show_render', 'var', 'SCENE', scene, 'mg_props.' + value[0], expression=value[1])
+                            add_driver_to(obj.modifiers[mod_name], 'show_viewport', 'var', 'SCENE', scene, 'mg_props.' + value[0], expression=value[1])
+                        else:
+                            add_driver_to(obj.modifiers[mod_name], prop, 'var', 'SCENE', scene, 'mg_props.' + value[0], expression=value[1])
+                elif hasattr(mod, prop):
+                    setattr(mod, prop, value)
 
 
 def add_driver_to(obj: bpy_types.Object, prop_to, var_name: str, id_type: str, _id, prop_from, expression: str = None) -> None:
@@ -376,6 +391,7 @@ def add_driver_to_vars(obj: bpy_types.Object, prop_to, variables, expression: st
             driver = obj.driver_add(prop_to[0], prop_to[1]).driver
         else:
             driver = obj.driver_add(prop_to).driver
+            ModifierManager.drivers.append((obj, prop_to))
         for i, var_prop in enumerate(variables):
             try:
                 var = driver.variables[i]
@@ -393,3 +409,15 @@ def add_driver_to_vars(obj: bpy_types.Object, prop_to, variables, expression: st
             driver.expression = expression
         else:
             driver.expression = variables[0][0]
+
+
+class ModifierManager:
+    drivers = []
+
+    def delete_all_drivers():
+        for driver_settings in ModifierManager.drivers:
+            try:
+                driver_settings[0].driver_remove(driver_settings[1])
+            except TypeError:
+                pass
+        ModifierManager.drivers = []
