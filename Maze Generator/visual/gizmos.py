cd406f52d5293f6f8e508bdc948290f5d8dd8d4e
Nathan Hild <nathan.hild@gmail.com>
2020-10-13 00:07:13 +0200

    Housekeeping

--- Maze Generator/visual/gizmos.py
+++ Maze Generator/visual/gizmos.py
@@ -1,14 +1,20 @@
-from ..managers.object_manager import ObjectManager as om
-from ..managers import modifier_manager as mm
+"""
+Module to handle add-on gizmos
+"""
+from math import radians
 from mathutils import Matrix
-from math import pi, radians
 
 from bpy.types import (
     GizmoGroup,
 )
+from ..managers.object_manager import ObjectManager as om
+from ..managers import modifier_manager as mm
 
 
 class MazeWidgetGroup(GizmoGroup):
+    """
+    Main Maze widget
+    """
     bl_idname = "OBJECT_GGT_light_test"
     bl_label = "Test Light Widget"
     bl_space_type = 'VIEW_3D'
@@ -33,15 +39,20 @@ class MazeWidgetGroup(GizmoGroup):
         new_widget.color = color
         new_widget.alpha = 1
 
-        new_widget.color_highlight = color[0] + 0.4, color[1] + 0.4, color[2] + 0.4
+        new_widget.color_highlight = color[0] + \
+            0.4, color[1] + 0.4, color[2] + 0.4
         new_widget.alpha_highlight = 0.5
-        self.register_widget(new_widget, Matrix.Rotation(radians(rot_angle), 4, rot_axis))
+        self.register_widget(new_widget, Matrix.Rotation(
+            radians(rot_angle), 4, rot_axis))
 
     def setup(self, context):
         mg_props = context.scene.mg_props
-        self.setup_widget(mg_props, "maze_columns_gizmo", color=(1, 0, 0), rot_axis='Y', rot_angle=90)
-        self.setup_widget(mg_props, "maze_rows_or_radius_gizmo", color=(0, 1, 0), rot_axis='X', rot_angle=-90)
-        self.setup_widget(om.obj_cells.modifiers[mm.M_STAIRS], "strength", color=(0, 0, 1))
+        self.setup_widget(mg_props, "maze_columns_gizmo",
+                          color=(1, 0, 0), rot_axis='Y', rot_angle=90)
+        self.setup_widget(mg_props, "maze_rows_or_radius_gizmo",
+                          color=(0, 1, 0), rot_axis='X', rot_angle=-90)
+        self.setup_widget(
+            om.obj_cells.modifiers[mm.M_STAIRS], "strength", color=(0, 0, 1))
         # self.setup_widget(om.obj_cells.modifiers[mm.M_THICKNESS_DISP], "strength", color=(0, 0, 1), rot_axis='X', rot_angle=180)
 
     def refresh(self, context):
--- Maze Generator/visual/maze_visual.py
+++ Maze Generator/visual/maze_visual.py
@@ -1,8 +1,16 @@
+"""
+This module should be deprecated soon enough
+"""
+
 import bpy
+from ..managers import algorithm_manager, texture_manager, mesh_manager, object_manager, grid_manager, modifier_manager
 from ..shading.objects import manager as mat_creator
 
 
 class MazeVisual:
+    """
+    This class does way too much things
+    """
     scene = None
     props = None
 
@@ -11,7 +19,10 @@ class MazeVisual:
         MazeVisual.props = scene.mg_props
         object_manager.ObjectManager.get_or_create_and_link_objects(scene)
         texture_manager.TextureManager.generate_textures(bpy.data.textures)
-        # material_manager.MaterialManager.set_materials(MazeVisual.props, scene)
+        mesh_manager.MeshManager.create_vertex_groups(
+            object_manager.ObjectManager.obj_cells, object_manager.ObjectManager.obj_walls)
+        modifier_manager.setup_modifiers_and_drivers(
+            MazeVisual, object_manager.ObjectManager, texture_manager.TextureManager)
 
     def generate_maze(scene: bpy.types.Scene) -> None:
         self = MazeVisual
@@ -21,7 +32,7 @@ class MazeVisual:
 
         mesh_manager.MeshManager.reset()
 
-        grid_manager.GridManager.generate_grid(self.props)
+        grid_manager.GridManager.generate_grid(props)
         grid_manager.GridManager.grid.mask_cells()
         grid_manager.GridManager.grid.prepare_grid()
         grid_manager.GridManager.grid.init_cells_neighbors()
@@ -30,18 +41,24 @@ class MazeVisual:
         if not algorithm_manager.is_algo_incompatible(props):
             algorithm_manager.work(grid_manager.GridManager.grid, props)
             grid_manager.GridManager.grid.calc_state()
-            grid_manager.GridManager.grid.sparse_dead_ends(props.sparse_dead_ends, props.seed)
-            grid_manager.GridManager.grid.braid_dead_ends(100 - props.keep_dead_ends, props.seed)
+            grid_manager.GridManager.grid.sparse_dead_ends(
+                props.sparse_dead_ends, props.seed)
+            grid_manager.GridManager.grid.braid_dead_ends(
+                100 - props.keep_dead_ends, props.seed)
 
             object_manager.ObjectManager.get_or_create_and_link_objects(scene)
-            object_manager.ObjectManager.update_wall_visibility(self.props, algorithm_manager.is_algo_weaved(self.props))
+            object_manager.ObjectManager.update_wall_visibility(
+                props, algorithm_manager.is_algo_weaved(props))
 
             texture_manager.TextureManager.generate_textures(bpy.data.textures)
 
-            grid_manager.GridManager.grid.calc_distances(self.props)
-            mesh_manager.MeshManager.create_vertex_groups(object_manager.ObjectManager.obj_cells, object_manager.ObjectManager.obj_walls)
-            mesh_manager.MeshManager.build_objects(props, grid_manager.GridManager.grid, object_manager.ObjectManager.obj_cells, object_manager.ObjectManager.obj_walls)
+            grid_manager.GridManager.grid.calc_distances(props)
+            mesh_manager.MeshManager.create_vertex_groups(
+                object_manager.ObjectManager.obj_cells, object_manager.ObjectManager.obj_walls)
+            mesh_manager.MeshManager.build_objects(
+                props, grid_manager.GridManager.grid, object_manager.ObjectManager.obj_cells, object_manager.ObjectManager.obj_walls)
 
-            modifier_manager.setup_modifiers_and_drivers(MazeVisual, object_manager.ObjectManager, texture_manager.TextureManager)
+            modifier_manager.setup_modifiers_and_drivers(
+                MazeVisual, object_manager.ObjectManager, texture_manager.TextureManager)
 
             mat_creator.create_materials(props, object_manager.ObjectManager.obj_cells, object_manager.ObjectManager.obj_walls)
